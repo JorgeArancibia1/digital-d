9d658f3b1dd878cc9680c3e1c7ece69ac681b539
PabloGuinazu <50087392+PabloGuinazu@users.noreply.github.com>
2024-03-29 00:13:07 -0300

    Header (#2)
    * se añade hero, se añade config en tailwind
    
    * se añade hero, header y configuraciones varias
    
    * se realizan las modificaciones en los distintos archivos para que estos esten en tailwind, se añadio una carpeta al gitignore y .eslintrc, se modifico el menu item
    
    * Update settings.json
    
    * Se añade borra mod. en tailwind.config, se arreglan espacios en hero y menuitemview, se mod. el min-width en menucontainer
    
    * Se añade tarea a futuro
    
    ---------
    
    Co-authored-by: Jorge Arancibia <jorge.arancibia.jeal@gmail.com>

---
+++ src/data/menuItems.ts
@@ -0,0 +1,18 @@
+import type { menuItem } from '../interfaces';
+
+const menuItems: menuItem[] = [
+	{
+		title: 'Inicio',
+		url: '/',
+	},
+	{
+		title: 'Nosotros',
+		url: '/Nosotros',
+	},
+	{
+		title: 'Contactanos',
+		url: '/Contactanos',
+	},
+];
+
+export default menuItems;
