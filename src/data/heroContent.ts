76e2c71686dae8896b506adf57482fab0ffda11c
Pablo <guinazuzu@gmail.com>
2024-04-03 00:49:21 -0300

    se configuro prettier para formatear todos los archivos modificados

--- src/data/heroContent.ts
+++ src/data/heroContent.ts
@@ -1,12 +1,12 @@
 interface IHeroContent {
-	title: string;
-	description: string;
+  title: string;
+  description: string;
 }
 
 export const heroContent: IHeroContent[] = [
-	{
-		title: 'Titulos prueba',
-		description:
-			'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
-	},
+  {
+    title: 'Titulos prueba',
+    description:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+  },
 ];
